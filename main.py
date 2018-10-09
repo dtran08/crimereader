85fb423027975c7549261bc880ecb18da79749a7
Douglas Tran <doug.tran.8@gmail.com>
2018-10-09 14:59:12 -0700

    Fixing display bugs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from os import path
 
 input_list=[]
 
-print(f'1:incident_id\n2:case_number\n3:incident_datetime\n4:incident_type_primary\n5:incident_description\n6:clearance_type\n7:address_1\n8:address_2\n9:city\n10:state\n11:zip\n12:country\n14:latitude\n15:longitude\n16:created_at\n17:updated_at\n18:location\n19:hour_of_day\nday_of_week\n20:parent_incident_type\n')
+print(f'1:incident_id\n2:case_number\n3:incident_datetime\n4:incident_type_primary\n5:incident_description\n6:clearance_type\n7:address_1\n8:address_2\n9:city\n10:state\n11:zip\n12:country\n13:latitude\n14:longitude\n15:created_at\n16:updated_at\n17:location\n18:hour_of_day\n19:day_of_week\n20:parent_incident_type\n')
 
 while 1:#userInput >= 1 and userInput <= 20:
 	try:
@@ -35,7 +35,46 @@ sortInput = int(input(f'Select the column you would like to sort by.\n'))
 if not sortInput in input_list:
 	print (f'Not in list.')
 elif sortInput in input_list:
-	print (f"In list. Sorting by",sortInput)
+	if sortInput == 1:
+		print (f"In list. Sorting by incident_id",sortInput)
+	if sortInput == 2:
+		print (f"In list. Sorting by case_number",sortInput)
+	if sortInput == 3:
+		print (f"In list. Sorting by incident_datetime",sortInput)
+	if sortInput == 4:
+		print (f"In list. Sorting by incident_type_primary",sortInput)
+	if sortInput == 5:
+		print (f"In list. Sorting by incident_description",sortInput)
+	if sortInput == 6:
+		print (f"In list. Sorting by clearance_type",sortInput)
+	if sortInput == 7:
+		print (f"In list. Sorting by address_1",sortInput)
+	if sortInput == 8:
+		print (f"In list. Sorting by address_2",sortInput)
+	if sortInput == 9:
+		print (f"In list. Sorting by city",sortInput)
+	if sortInput == 10:
+		print (f"In list. Sorting by state",sortInput)
+	if sortInput == 11:
+		print (f"In list. Sorting by zip",sortInput)
+	if sortInput == 12:
+		print (f"In list. Sorting by country",sortInput)
+	if sortInput == 13:
+		print (f"In list. Sorting by latitude",sortInput)
+	if sortInput == 14:
+		print (f"In list. Sorting by longitude",sortInput)
+	if sortInput == 15:
+		print (f"In list. Sorting by created_at",sortInput)
+	if sortInput == 16:
+		print (f"In list. Sorting by updated_at",sortInput)
+	if sortInput == 17:
+		print (f"In list. Sorting by location",sortInput)
+	if sortInput == 18:
+		print (f"In list. Sorting by hour_of_day",sortInput)
+	if sortInput == 19:
+		print (f"In list. Sorting by day_of_week",sortInput)
+	if sortInput == 20:
+		print (f"In list. Sorting by parent_incident_type",sortInput)
 
 # out = pandas.read_csv('Riverside_County_Sheriff_Department.csv')
 
