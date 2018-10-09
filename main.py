4a317af4796a8b0ac9fe2d3c98b5b664d268ac32
Douglas Tran <doug.tran.8@gmail.com>
2018-10-08 19:31:03 -0700

    Sorting input

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+#To Do
+# -link array to sections
+# -figure out sorting
+
 import csv
 import pandas
 
-#BUGS TO FIX:
-# -entering anything besides an int will crash the loop
-# -repeats are not working
-# -you can only exit the loop by entering something out of range
-
 input_list=[]
+
+print(f'1:incident_id\n2:case_number\n3:incident_datetime\n4:incident_type_primary\n5:incident_description\n6:clearance_type\n7:address_1\n8:address_2\n9:city\n10:state\n11:zip\n12:country\n14:latitude\n15:longitude\n16:created_at\n17:updated_at\n18:location\n19:hour_of_day\nday_of_week\n20:parent_incident_type\n')
 
 while 1:#userInput >= 1 and userInput <= 20:
 	try:
@@ -26,7 +27,16 @@ while 1:#userInput >= 1 and userInput <= 20:
 	else:
 		input_list.append(userInput)
 
-print(f"Printing list...\n")
-print(*input_list, sep='\n')
-print(f"Success")
+print(f'Printing list...')
+print(*input_list)
+
+sortInput = int(input(f'Select the column you would like to sort by.\n'))
+if not sortInput in input_list:
+	print (f'Not in list.')
+elif sortInput in input_list:
+	print (f"In list. Sorting by",sortInput)
+
+# out = pandas.read_csv('Riverside_County_Sheriff_Department.csv')
+
+
 
